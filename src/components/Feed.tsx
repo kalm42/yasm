a9af577189c0f3f21dafa7ed9afcaf6533afa9c8
Kyle Melton <kalm42@gmail.com>
2021-01-27 15:16:00 -0800

    func: feed only shows posts from followed users

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,6 +1,8 @@
 import { firestore } from "../services/firebase";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import Post from "./Post";
+import { useUser } from "../context";
+import { useEffect, useState } from "react";
 
 interface PostInterface {
   profileImage?: string;
@@ -15,14 +17,58 @@ interface PostInterface {
 }
 
 const Feed = () => {
-  const postsRef = firestore.collection("posts");
-  const query = postsRef.orderBy("createdAt").limit(25);
-  const [posts] = useCollectionData<PostInterface>(query, { idField: "id" });
+  const { user } = useUser();
+  const [followList, setFollowList] = useState<string[]>([]);
+  const [posts, setPosts] = useState<PostInterface[]>([]);
+  // get users I follow
+  // get posts those users have made
 
-  if (!posts?.length) return null;
+  useEffect(() => {
+    const getFollowed = async () => {
+      if (!user) return;
+      const followsRef = firestore.collection("follows");
+      const query = await followsRef
+        .where("follower", "==", user?.uid)
+        .get()
+        .then((querySnapshot) => {
+          let follows: string[] = [];
+          querySnapshot.forEach((doc) => {
+            const data = doc.data();
+            follows.push(data.followed);
+          });
+          return follows;
+        });
+      setFollowList(query);
+    };
+    getFollowed();
+  }, [user]);
+
+  useEffect(() => {
+    // get posts from those users
+    const getPosts = async () => {
+      if (!followList.length) return;
+      console.log(followList);
+      const postsRef = firestore.collection("posts");
+      const query = postsRef
+        .where("authorId", "in", followList)
+        .orderBy("createdAt", "desc")
+        .limit(25);
+      query.get().then((querySnapshot) => {
+        const p: PostInterface[] = [];
+        querySnapshot.forEach((doc) => {
+          const q = doc.data();
+          q.id = doc.id;
+          p.push(q as PostInterface);
+        });
+        setPosts(p);
+      });
+    };
+    getPosts();
+  }, [followList]);
 
   return (
     <section>
+      <h2>the feed</h2>
       {posts.map((post) => (
         <Post key={post.id} {...post} />
       ))}
--- src/components/WritePost.tsx
+++ src/components/WritePost.tsx
@@ -1,32 +1,47 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import firebase from "firebase/app";
-import { auth, firestore } from "../services/firebase";
+import { firestore } from "../services/firebase";
+import { useUser } from "../context";
+import { ExtendedUser } from "../models/posts";
 
 const WritePost = () => {
   const [post, setPost] = useState("");
+  const [author, setAuthor] = useState<ExtendedUser | null>(null);
+  const { user } = useUser();
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
-    if (!auth.currentUser) {
-      // TODO: show some error regarding not being logged in.
-      return;
-    }
+    if (!user || !author) return;
     const postsRef = firestore.collection("posts");
-    const { uid, photoURL, displayName } = auth.currentUser;
 
     try {
       await postsRef.add({
         text: post,
         createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-        authorId: uid,
-        authorName: displayName,
-        photoURL,
+        authorId: user.uid,
+        authorAt: author.id,
+        authorName: user.displayName,
+        photoURL: user.photoURL,
       });
       setPost("");
     } catch (error) {
       console.warn(`WritePost Error: ${error.message}`);
     }
   };
+
+  // get current user details
+  useEffect(() => {
+    const getUser = async () => {
+      if (!user) return;
+      const u = await firestore.doc(`users/${user.uid}`).get();
+      const p = u.data();
+      if (p) {
+        p.uid = u.id;
+        setAuthor(p as ExtendedUser);
+      }
+    };
+    getUser();
+  }, [user]);
 
   return (
     <form onSubmit={handleSubmit}>
