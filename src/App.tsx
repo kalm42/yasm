c330ff2a3f8d5768b7aeccba5276d96ba7e22c16
Kyle Melton <kalm42@gmail.com>
2021-02-02 08:03:03 -0800

    fix: remove unused pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import * as Sentry from "@sentry/react";
 import Layout from "./components/Layout";
 import { ApplicationContext } from "./context";
-import Auth from "./pages/Auth";
 import Bookmarks from "./pages/Bookmarks";
 import Home from "./pages/Home";
 import Notifications from "./pages/Notifications";
 import Post from "./pages/Post";
 import Profile from "./pages/Profile";
-import Settings from "./pages/Settings";
 
 function App() {
   return (
@@ -19,17 +17,11 @@ function App() {
           <BrowserRouter>
             <Layout>
               <Switch>
-                <Route path="/auth">
-                  <Auth />
-                </Route>
                 <Route path="/bookmarks">
                   <Bookmarks />
                 </Route>
                 <Route path="/notifications">
                   <Notifications />
-                </Route>
-                <Route path="/settings">
-                  <Settings />
                 </Route>
                 <Route path="/post/:id">
                   <Post />
