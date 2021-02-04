b3042b614d6691f6107c0e903c1b174268d72b37
Kyle Melton <1078490+kalm42@users.noreply.github.com>
2021-02-04 14:33:04 -0800

    add styles

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,24 +1,9 @@
 import React, { ReactNode } from "react";
 import * as Sentry from "@sentry/react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
 import { useUser } from "../context";
 import UserCard from "./UserCard";
-
-const Base = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 3fr;
-  min-height: calc(100vh);
-`;
-
-const Header = styled.header`
-  display: flex;
-  flex-direction: column;
-  position: sticky;
-  top: 0;
-  left: 0;
-  max-height: calc(100vh);
-`;
+import "./Layout.css";
 
 interface Props {
   children: ReactNode;
@@ -29,10 +14,9 @@ const Layout = (props: Props) => {
   const { user } = useUser();
   return (
     <Sentry.ErrorBoundary fallback={FallbackLayout}>
-      <Base>
-        <Header>
-          <p>YASM</p>
-          <nav style={{ flex: 1 }}>
+      <div className="layout__root">
+        <header className="layout__header">
+          <nav className="layout__nav">
             <ul>
               <li>
                 <Link to="/">Home</Link>
@@ -53,9 +37,9 @@ const Layout = (props: Props) => {
             </ul>
           </nav>
           <UserCard />
-        </Header>
+        </header>
         <main>{children}</main>
-      </Base>
+      </div>
     </Sentry.ErrorBoundary>
   );
 };
